e10e2c9fce94ae18ab4196b1e247f5422fec5838
uerceg <ugi@adjust.com>
2019-10-09 14:27:21 +0200

    Example and test app stuff

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/uerceg/Development/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/uerceg/Github/flutter_sdk_dev/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/uerceg/Development/Flutter/flutter/bin/cache/artifacts/engine/ios"
