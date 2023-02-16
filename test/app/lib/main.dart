61c6e768f6ec04b136f1ecc91f3cf29dbf72d9ae
uerceg <ugi@adjust.com>
2023-02-16 13:29:29 +0100

    chore: update example and test apps

--- test/app/lib/main.dart
+++ test/app/lib/main.dart
@@ -27,7 +27,7 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
 
-    String _address = '192.168.86.44';
+    String _address = '192.168.8.184';
     if (Platform.isAndroid) {
       String _protocol = 'https';
       String _port = '8443';
