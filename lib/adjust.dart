fa91e8df28a622b42b4d0a30f8c59945bcd81e79
uerceg <ugi@adjust.com>
2024-07-10 13:03:40 +0200

    feat: update version number to 4.38.2

--- lib/adjust.dart
+++ lib/adjust.dart
@@ -23,7 +23,7 @@ import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
 
 class Adjust {
-  static const String _sdkPrefix = 'flutter4.38.1';
+  static const String _sdkPrefix = 'flutter4.38.2';
   static const MethodChannel _channel =
       const MethodChannel('com.adjust.sdk/api');
 
