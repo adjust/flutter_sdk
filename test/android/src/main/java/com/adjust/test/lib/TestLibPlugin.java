336f53e38c71f3cb54672f93ce08612de4e7aab8
uerceg <ugi@adjust.com>
2024-12-05 23:46:16 +0100

    test: update android test app

--- test/android/src/main/java/com/adjust/test/lib/TestLibPlugin.java
+++ test/android/src/main/java/com/adjust/test/lib/TestLibPlugin.java
@@ -12,12 +12,13 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import com.adjust.test.ICommandJsonListener;
 import com.adjust.test.TestLibrary;
@@ -30,48 +31,21 @@ public class TestLibPlugin implements FlutterPlugin, MethodCallHandler {
     private static String TAG = "TestLibPlugin";
     private TestLibrary testLibrary = null;
     private MethodChannel channel;
-    private boolean v2Plugin;
-    private boolean v2Attached = false;
 
-    private TestLibPlugin(MethodChannel channel) {
-        this.channel = channel;
+    public TestLibPlugin() {}
 
-        v2Plugin = false;
-    }
-
-    public TestLibPlugin() {
-        v2Plugin = true;
-    }
-
-    // FlutterPlugin
     @Override
-    public void onAttachedToEngine(FlutterPluginBinding binding) {
-        if (!v2Plugin) {
-            return;
-        }
-        if (v2Attached) {
-            return;
-        }
-
-        v2Attached = true;
-
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
         channel = new MethodChannel(binding.getBinaryMessenger(), "com.adjust.test.lib/api");
         channel.setMethodCallHandler(this);
     }
 
     @Override
-    public void onDetachedFromEngine(FlutterPluginBinding binding) {
-        v2Attached = false;
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
         if (channel != null) {
             channel.setMethodCallHandler(null);
         }
         channel = null;
-    }
-
-    // Plugin registration.
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "com.adjust.test.lib/api");
-        channel.setMethodCallHandler(new TestLibPlugin(channel));
     }
 
     @Override
